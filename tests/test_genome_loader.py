0e785dc6d343cdca2d036ff2a3f3160539b2c196
ialarmedalien <ialarmedalien@gmail.com>
2025-01-10 14:18:34 -0800

    reformatting;

--- tests/test_genome_loader.py
+++ tests/test_genome_loader.py
@@ -3,8 +3,7 @@ import unittest
 import shutil
 import csv
 
-from  genome_loader_scripts.genome_loader import MultiGenomeDataFileCreator
-
+from genome_loader_scripts.genome_loader import MultiGenomeDataFileCreator
 
 
 class TestGenomeDataFileCreation(unittest.TestCase):
@@ -16,9 +15,6 @@ class TestGenomeDataFileCreation(unittest.TestCase):
         # Remove the output directory if it exists
         if os.path.exists(self.output_dir):
             shutil.rmtree(self.output_dir)
-
-
-
 
     def test_file_creation(self):
         # check file creation
@@ -36,55 +32,67 @@ class TestGenomeDataFileCreation(unittest.TestCase):
             "tests/output/structural_annotation.tsv": 3,
         }
 
-        print ("\nTest: Check file creation")
+        print("\nTest: Check file creation")
         for file in self.files_and_expected_lines:
             expected_lines = self.files_and_expected_lines[file]
-            print (f"checking if number of file lines in {file} is equal to  {expected_lines}")
+            print(f"checking if number of file lines in {file} is equal to  {expected_lines}")
             self.assertTrue(os.path.exists(file), f"{file} was not created.")
 
-    @unittest.skip("Skipping checkm2  test")
+    @unittest.skip("Skipping checkm2 test")
     def test_checkm2(self):
         # check file creation
         # Initialize the creators for each test case
 
-        print ("\nTest: Includes Checkm2 run")
+        print("\nTest: Includes Checkm2 run")
         self.feature_protein_creator = MultiGenomeDataFileCreator(
             self.genome_paths_file, self.output_dir, 1
         )
         self.feature_protein_creator.create_all_tables()
 
         self.expected_scores = {
-            "423e40e5b4056069f9b0bfb71a3c682b41a8b68200b617a6e19902fa5dac7e94": {"contamination": 1.09, "completeness": 99.99},
-            "b32625f62ae333d2290a989cef9b3db75f462aeee8543aa11190af7f41c1d931": {"contamination": 2.27, "completeness": 99.97},
+            "423e40e5b4056069f9b0bfb71a3c682b41a8b68200b617a6e19902fa5dac7e94": {
+                "contamination": 1.09,
+                "completeness": 99.99,
+            },
+            "b32625f62ae333d2290a989cef9b3db75f462aeee8543aa11190af7f41c1d931": {
+                "contamination": 2.27,
+                "completeness": 99.97,
+            },
         }
         self.contigset_file = os.path.join(self.output_dir, "contigset.tsv")
 
-        with open(self.contigset_file, 'r') as tsvfile:
-            reader = csv.DictReader(tsvfile, delimiter='\t')
-            
-            for row in reader:
-                contigset_hash = row['contigset_hash']
-                
-                contamination = float(row['checkm2_contamination'])
-                completeness = float(row['checkm2_completeness'])
+        with open(self.contigset_file, "r") as tsvfile:
+            reader = csv.DictReader(tsvfile, delimiter="\t")
 
-                print (f"contamination for {contigset_hash} is {contamination}")
-                print (f"completenessfor {contigset_hash} is {completeness}")
+            for row in reader:
+                contigset_hash = row["contigset_hash"]
+
+                contamination = float(row["checkm2_contamination"])
+                completeness = float(row["checkm2_completeness"])
+
+                print(f"contamination for {contigset_hash} is {contamination}")
+                print(f"completenessfor {contigset_hash} is {completeness}")
 
                 if contigset_hash in self.expected_scores:
-                    expected_contamination = self.expected_scores[contigset_hash]['contamination']
-                    expected_completeness = self.expected_scores[contigset_hash]['completeness']
+                    expected_contamination = self.expected_scores[contigset_hash]["contamination"]
+                    expected_completeness = self.expected_scores[contigset_hash]["completeness"]
 
                     # Assert contamination and completeness match the expected values
-                    self.assertAlmostEqual(contamination, expected_contamination, places=2, 
-                                           msg=f"Contamination score mismatch for contigset hash {contigset_hash}")
-                    self.assertAlmostEqual(completeness, expected_completeness, places=2, 
-                                           msg=f"Completeness score mismatch for contigset hash {contigset_hash}")
+                    self.assertAlmostEqual(
+                        contamination,
+                        expected_contamination,
+                        places=2,
+                        msg=f"Contamination score mismatch for contigset hash {contigset_hash}",
+                    )
+                    self.assertAlmostEqual(
+                        completeness,
+                        expected_completeness,
+                        places=2,
+                        msg=f"Completeness score mismatch for contigset hash {contigset_hash}",
+                    )
                 else:
                     self.fail(f"Unexpected contig_hash found in TSV file: {contig_hash}")
 
 
-
 if __name__ == "__main__":
     unittest.main()
-
