f6b793d2d6e68948329bc434cd29dcaccd332175
alinakbase <ywang4196@lbl.gov>
2025-07-21 16:47:43 -0700

    Update test_uniprot.py

--- tests/test_uniprot.py
+++ tests/test_uniprot.py
@@ -20,10 +20,6 @@ How to run in the terminal:
 
 """
 
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
-
 import re
 import pytest
 import xml.etree.ElementTree as ET
